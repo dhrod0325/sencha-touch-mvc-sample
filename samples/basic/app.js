8524e2ba427749655519a01f104a7aeeb7b6ceea
dhrod0325 <dhrod0325@naver.com>
2014-01-18 06:03:50 +0900

    asdf

--- samples/basic/app.js
+++ samples/basic/app.js
@@ -1,15 +1,17 @@
 Ext.Loader.setPath({
-    'TestMVC' : 'samples/basic/app'
+    'TestMVC': 'samples/basic/app'
 });
 
 Ext.application({
-    name : 'testMVC',
-    requires : ['Ext.Container', 'Ext.Button', 'Ext.Toolbar', 'Ext.TitleBar', 'Ext.MessageBox', 'Ext.XTemplate'],
-    controllers : ['TestMVC.controller.TestController'],
-    models : ['TestMVC.model.Man'],
-    stores : ['TestMVC.store.Mans'],
-    views : ['TestMVC.view.Main'],
-    launch : function() { 
+    name: 'testMVC',
+    requires: ['Ext.Container', 'Ext.Button', 'Ext.Toolbar', 'Ext.TitleBar', 'Ext.MessageBox', 'Ext.XTemplate'],
+    controllers: ['TestMVC.controller.TestController'],
+    models: ['TestMVC.model.Man'],
+    stores: ['TestMVC.store.Mans'],
+    views: ['TestMVC.view.Main'],
+    launch: function () {
         Ext.Viewport.add(Ext.create('TestMVC.view.Main'));
     }
 });
+
+
